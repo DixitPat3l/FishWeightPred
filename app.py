a11c3eb53d7206f7df56c3920b04916f79c67f45
Dixit Patel <dixit.patel1@dcmail.ca>
2024-07-17 17:03:15 -0400

    Third commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_file
 import joblib
 import numpy as np
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,5 @@ def histograms():
     return send_file('histograms.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
